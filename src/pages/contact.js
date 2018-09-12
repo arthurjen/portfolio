ac5e08c6c4deef6cdf8d4c5c6e3a46766ddb2d56
Arthur Jen <arthurljen@gmail.com>
2018-09-12 14:35:52 -0700

    split up index into pages

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default () => (
+  <section id="contact">
+    <header>
+      <h2>let's connect</h2>
+    </header>
+    <ul>
+      <li>
+        Email
+      </li>
+      <li>
+        Linkedin
+      </li>
+      <li>
+        GitHub
+      </li>
+      <li>
+        Steam
+      </li>
+    </ul>
+  </section>
+)
