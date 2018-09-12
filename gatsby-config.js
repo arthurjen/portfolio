355bc0aafa4b6e010547d333915f5c58463b10c3
Arthur Jen <arthurljen@gmail.com>
2018-09-12 13:22:51 -0700

    installed dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Arthur Jen',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `81l3jtc3jps0`,
+        accessToken: `5362989d9b7d8cf1260e71fca5340bf6ce10c3999f3271c4b5e74aa7aab9df93`,
+      },
+    }
+  ]
 }
