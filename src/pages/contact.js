db1d1300e25ce691cdc40c2194cbf47e35c88c09
Arthur Jen <arthurljen@gmail.com>
2018-09-16 11:31:57 -0700

    contacts page complete

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,23 +5,32 @@ import styles from './contact.scss';
 export default () => (
   <Layout>
     <section className={styles.contact}>
-      <header>
-        <h2>let's connect</h2>
-      </header>
-      <ul>
-        <li>
-          Email
-        </li>
-        <li>
-          Linkedin
-        </li>
-        <li>
-          GitHub
-        </li>
-        <li>
-          Steam
-        </li>
-      </ul>
+      <div>
+        <header>
+          <h2>let's <span>connect</span></h2>
+        </header>
+        <ul>
+          <li>
+          <a href="mailto:arthurljen@gmail.com">Email</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/arthurjen/">Linkedin</a>
+          </li>
+          <li>
+            <a href="https://github.com/arthurjen">Github</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <header>
+          <h2>or let's <span>play</span></h2>
+        </header>
+        <ul>
+          <li>
+            <a href="https://steamcommunity.com/id/wallaroo">Steam</a>
+          </li>
+        </ul>
+      </div>
     </section>
   </Layout>
 )
