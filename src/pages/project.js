3bfd119840234860dd80dbaff1c326c26e2cd397
Arthur Jen <arthurljen@gmail.com>
2018-09-16 20:01:16 -0700

    added favicon

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/layout';
 import styles from './project.scss';
 
-export default () => (
-  <Layout>
-    <section className={styles.project}>
-      <div>
-        <header>
-          <h2>Project <span>TITLE</span></h2>
-        </header>
-        <p>about the project</p>
-        <img src="something" alt="project photo"/>>
-      </div>
-    </section>
-  </Layout>
-)
+class Project extends Component {
+  state = {  }
+  render() { 
+    return (
+      <Layout>
+        <section className={styles.project}>
+          <div>
+            <header>
+              <h2>Booxy</h2>
+            </header>
+            <p>about the project</p>
+            <h1> AJ </h1>
+            <img src="something" alt="project photo"/>>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
+ 
+export default Project;
