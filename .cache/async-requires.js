4f710aeead6ef8e5d0469386cd8a4258be452d4c
Arthur Jen <arthurljen@gmail.com>
2018-09-16 20:29:36 -0700

    something

---
+++ .cache/async-requires.js
@@ -0,0 +1,13 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/arthurjen/alchemy/401/portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-project-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/project.js" /* webpackChunkName: "component---src-pages-project-js" */)
+}
+
+exports.data = () => import("/Users/arthurjen/alchemy/401/portfolio/.cache/data.json")
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/.cache/dev-404-page.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/about.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/contact.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/index.js")),
+  "component---src-pages-project-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/project.js"))
+}
+
