7bdcd10557be094b20338414eb98a8ebc9cf2fa3
Arthur Jen <arthurljen@gmail.com>
2018-10-16 13:06:23 -0700

    no more menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import styles from './Header.scss';
 
-const activeLink = {
-  borderBottom: '4px solid rgb(21, 187, 253)'
-}
-
 
 class Header extends Component {
-  state = {  }
+  state = {
+    menu: false
+  };
+
   render() { 
+
+    const activeLink = {
+      borderBottom: '1px solid rgb(21, 187, 253)',
+      borderRight: '1px solid rgb(21, 187, 253)'
+    }
+
     return (
       <header className={styles.header}>
         <h1>Arthur Jen</h1>
-        <div>
-
-        </div>
-        <i className="fas fa-bars" onClick={() => this.setState({ menu: true })}></i>
         <nav>
           <Link activeStyle={activeLink} to="/">WORK</Link>
-          <Link activeStyle={activeLink} to="/about">ABOUT</Link>
+          <Link activeStyle={activeLink} to="/about/">ABOUT</Link>
         </nav>
       </header>
     );
