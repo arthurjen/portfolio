8ca1fa88e0a124c076d10e4efc0e860bc159e965
Arthur Jen <arthurljen@gmail.com>
2018-10-16 12:48:31 -0700

    header changes between icon and words

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import styles from './Header.scss';
 
@@ -6,15 +6,25 @@ const activeLink = {
   borderBottom: '4px solid rgb(21, 187, 253)'
 }
 
-export default () => (
-  <header className={styles.header}>
-    <hgroup>
-      <h1>Arthur Jen</h1>
-    </hgroup>
-    
-    <nav>
-      <Link activeStyle={activeLink} to="/">Work</Link>
-      <Link activeStyle={activeLink} to="/about">About</Link>
-    </nav>
-  </header>
-)
+
+class Header extends Component {
+  state = {  }
+  render() { 
+    return (
+      <header className={styles.header}>
+        <h1>Arthur Jen</h1>
+        <div>
+
+        </div>
+        <i className="fas fa-bars" onClick={() => this.setState({ menu: true })}></i>
+        <nav>
+          <Link activeStyle={activeLink} to="/">WORK</Link>
+          <Link activeStyle={activeLink} to="/about">ABOUT</Link>
+        </nav>
+      </header>
+    );
+  }
+}
+ 
+export default Header;
+  
