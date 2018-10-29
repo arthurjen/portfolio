ece10985ae029cf8353066664271d980aafe8088
Arthur Jen <arthurljen@gmail.com>
2018-10-29 16:13:07 -0700

    added mouseover animation to index

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,7 +4,8 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/arthurjen/alchemy/401/portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-about-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-contact-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-companion-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/companion.js" /* webpackChunkName: "component---src-pages-companion-js" */),
+  "component---src-pages-gitgrub-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/gitgrub.js" /* webpackChunkName: "component---src-pages-gitgrub-js" */),
   "component---src-pages-halchemy-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/halchemy.js" /* webpackChunkName: "component---src-pages-halchemy-js" */),
   "component---src-pages-index-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-snakes-js": () => import("/Users/arthurjen/alchemy/401/portfolio/src/pages/snakes.js" /* webpackChunkName: "component---src-pages-snakes-js" */)
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,8 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/.cache/dev-404-page.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/about.js")),
-  "component---src-pages-contact-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/contact.js")),
+  "component---src-pages-companion-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/companion.js")),
+  "component---src-pages-gitgrub-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/gitgrub.js")),
   "component---src-pages-halchemy-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/halchemy.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/index.js")),
   "component---src-pages-snakes-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/snakes.js"))
