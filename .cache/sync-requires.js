0ed4df47a98f938e1b1861cb480f54eb200122f3
Arthur Jen <arthurljen@gmail.com>
2018-10-29 21:52:53 -0700

    gatsby build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/.cache/dev-404-page.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/about.js")),
   "component---src-pages-contact-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/contact.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/arthurjen/alchemy/401/portfolio/src/pages/index.js")),
