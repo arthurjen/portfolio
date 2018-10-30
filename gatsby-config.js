d03aa7cbd669fe5f1800b324c2ed2c9d497fff80
Arthur Jen <arthurljen@gmail.com>
2018-10-30 11:09:06 -0700

    gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
-const { SPACE_ID, ACCESS_TOKEN } = process.env;
+// require('dotenv').config();
+// const { SPACE_ID, ACCESS_TOKEN } = process.env;
 module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -14,13 +12,13 @@ module.exports = {
         path: `${__dirname}/src/assets/`
       }
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: SPACE_ID,
-        accessToken: ACCESS_TOKEN,
-        host: `preview.contentful.com`,
-      },
-    }
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: SPACE_ID,
+    //     accessToken: ACCESS_TOKEN,
+    //     host: `preview.contentful.com`,
+    //   },
+    // }
   ]
 };
