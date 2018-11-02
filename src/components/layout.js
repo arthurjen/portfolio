e3328bbebab2e640b9b483d39ea270dee8b43018
Arthur Jen <arthurljen@gmail.com>
2018-11-02 12:37:55 -0700

    gatsby build

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,6 @@ import React from "react";
 import Helmet from 'react-helmet';
 import Header from './Header';
 
-import "./layout.css";
-
 export default ({ children }) => 
 <div>
   <Helmet defaultTitle={`Arthur Jen`}>
