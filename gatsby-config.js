37656c2760c874d46b563d6cd1f3c910ff89acf9
Arthur Jen <arthurljen@gmail.com>
2018-11-02 12:01:52 -0700

    installed gatsby eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,20 +5,13 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-favicon`,
+    `gatsby-plugin-eslint`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
         path: `${__dirname}/src/assets/`
       }
-    },
-    // {
-    //   resolve: `gatsby-source-contentful`,
-    //   options: {
-    //     spaceId: SPACE_ID,
-    //     accessToken: ACCESS_TOKEN,
-    //     host: `preview.contentful.com`,
-    //   },
-    // }
+    }
   ]
 };
